65d994ec6069bb5625f7942270b338a4f2e336b3
Hernan Ariel De Luca <hadeluca@gmail.com>
2020-07-22 17:13:12 +0200

    modify folder structure for library and applications test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
-import { LtCodemirrorModule } from '../lt-codemirror/lt-codemirror.module';
+import { LtCodemirrorModule } from 'lt-codemirror';
 
 @NgModule({
   declarations: [
